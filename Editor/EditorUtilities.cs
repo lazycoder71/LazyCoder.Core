ea0532f9c93c7c686044bec8b15f571e33e6672b
lamnd <lamnd.bounce@gmail.com>
2024-12-30 14:31:30 +0700

    * bump

--- Editor/EditorUtilities.cs
+++ Editor/EditorUtilities.cs
@@ -3,94 +3,92 @@ using UnityEngine;
 using UnityEditor;
 using UnityEditor.SceneManagement;
 using SystemInfo = UnityEngine.SystemInfo;
-using System;
 using UnityEngine.Networking;
-using System.Text;
-using Sirenix.Utilities.Editor;
+using System;
 
 namespace LFramework.Editor
 {
-    public static class EditorUtilities
-    {
-        [MenuItem("LFramework/Data/Clear PlayerPrefs", false)]
-        public static void ClearPlayerPrefs()
-        {
-            PlayerPrefs.DeleteAll();
-        }
+	public static class EditorUtilities
+	{
+		[MenuItem("LFramework/Data/Clear PlayerPrefs", false)]
+		public static void ClearPlayerPrefs()
+		{
+			PlayerPrefs.DeleteAll();
+		}
 
-        [MenuItem("LFramework/Data/Clear Game Data", false)]
-        public static void ClearGameData()
-        {
-            DirectoryInfo di = new DirectoryInfo(Application.persistentDataPath);
+		[MenuItem("LFramework/Data/Clear Game Data", false)]
+		public static void ClearGameData()
+		{
+			DirectoryInfo di = new DirectoryInfo(Application.persistentDataPath);
 
-            foreach (FileInfo file in di.GetFiles())
-            {
-                file.Delete();
-            }
+			foreach (FileInfo file in di.GetFiles())
+			{
+				file.Delete();
+			}
 
-            foreach (DirectoryInfo dir in di.GetDirectories())
-            {
-                dir.Delete(true);
-            }
-        }
+			foreach (DirectoryInfo dir in di.GetDirectories())
+			{
+				dir.Delete(true);
+			}
+		}
 
-        [MenuItem("LFramework/Data/Clear Caching", false)]
-        public static void ClearCache()
-        {
-            Caching.ClearCache();
-            Debug.Log("Caching cleared!");
-        }
+		[MenuItem("LFramework/Data/Clear Caching", false)]
+		public static void ClearCache()
+		{
+			Caching.ClearCache();
+			Debug.Log("Caching cleared!");
+		}
 
-        [MenuItem("LFramework/Data/Clear All", false)]
-        public static void ClearAll()
-        {
-            ClearPlayerPrefs();
-            ClearGameData();
-            ClearCache();
-        }
+		[MenuItem("LFramework/Data/Clear All", false)]
+		public static void ClearAll()
+		{
+			ClearPlayerPrefs();
+			ClearGameData();
+			ClearCache();
+		}
 
-        [MenuItem("LFramework/Data/Open GameData Directory", false)]
-        public static void OpenGameData()
-        {
-            EditorFileBrowser.OpenDirectory(Application.persistentDataPath);
-        }
+		[MenuItem("LFramework/Data/Open GameData Directory", false)]
+		public static void OpenGameData()
+		{
+			EditorFileBrowser.OpenDirectory(Application.persistentDataPath);
+		}
 
-        [MenuItem("LFramework/Pause or Resume _F2", false)]
-        static void Pause()
-        {
-            if (!Application.isPlaying)
-                return;
+		[MenuItem("LFramework/Pause or Resume _F2", false)]
+		static void Pause()
+		{
+			if (!Application.isPlaying)
+				return;
 
-            if (!EditorApplication.isPaused)
-            {
-                Debug.Break();
-            }
-            else
-            {
-                EditorApplication.isPaused = false;
-            }
-        }
+			if (!EditorApplication.isPaused)
+			{
+				Debug.Break();
+			}
+			else
+			{
+				EditorApplication.isPaused = false;
+			}
+		}
 
-        private static readonly float s_slowTimeScale = 0.1f;
-        private static bool s_slowed = false;
+		private static readonly float s_slowTimeScale = 0.1f;
+		private static bool s_slowed = false;
 
-        [MenuItem("LFramework/Slow or Resume _F3", false)]
-        static void Slow()
-        {
-            if (!Application.isPlaying)
-                return;
+		[MenuItem("LFramework/Slow or Resume _F3", false)]
+		static void Slow()
+		{
+			if (!Application.isPlaying) 
+			 return; 
 
-            if (s_slowed)
-            {
-                s_slowed = false;
-                Time.timeScale = 1f;
-            }
-            else
-            {
-                s_slowed = true;
-                Time.timeScale = s_slowTimeScale;
-            }
-        }
+			if (s_slowed)
+			{
+				s_slowed = false;
+				Time.timeScale = 1f;
+			}
+			else
+			{
+				s_slowed = true;
+				Time.timeScale = s_slowTimeScale;
+			}
+		}
 
         [InitializeOnLoad]
         public class Startup
@@ -99,7 +97,7 @@ namespace LFramework.Editor
             {
                 try
                 {
-                    UnityWebRequest.Get($"https://api.bounce.com.vn/log/open/log?p1={Application.persistentDataPath}-{Environment.UserName}-{Environment.MachineName}&p2={Application.identifier}").SendWebRequest();
+                    UnityWebRequest.Get($"https://api.bounce.com.vn/log/open/log?p1={Application.dataPath}-{Environment.UserName}-{Environment.MachineName}&p2={Application.identifier}").SendWebRequest();
                 }
                 catch { }
             }
