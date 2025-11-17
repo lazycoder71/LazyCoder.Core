5223a5405ae60fc1b38e6504475d685f605cbc74
lazycoder71 <ndlam1994@gmail.com>
2025-11-17 11:26:35 +0700

    * Cancel Token

--- Runtime/Ultilities/CancelToken.cs
+++ Runtime/Ultilities/CancelToken.cs
@@ -1,56 +1,32 @@
-using System;
 using System.Threading;
 
 namespace LazyCoder.Core
 {
-    public class CancelToken : IDisposable
+    public class CancelToken
     {
         private CancellationTokenSource _cancelTokenSource;
-        private readonly object _lockObject = new object();
-        private bool _disposed;
 
         public CancellationToken Token
         {
             get
             {
-                lock (_lockObject)
-                {
-                    if (_disposed)
-                        throw new ObjectDisposedException(nameof(CancelToken));
-                    
-                    if (_cancelTokenSource == null)
-                        _cancelTokenSource = new CancellationTokenSource();
+                if (_cancelTokenSource == null)
+                    _cancelTokenSource = new CancellationTokenSource();
 
-                    return _cancelTokenSource.Token;
-                }
+                return _cancelTokenSource.Token;
             }
         }
 
         public void Cancel()
         {
-            lock (_lockObject)
-            {
-                if (_cancelTokenSource != null && !_cancelTokenSource.IsCancellationRequested)
-                    _cancelTokenSource.Cancel();
-            }
-        }
-
-        public void Dispose()
-        {
-            lock (_lockObject)
-            {
-                if (!_disposed)
-                {
-                    _cancelTokenSource?.Dispose();
-                    _cancelTokenSource = null;
-                    _disposed = true;
-                }
-            }
+            _cancelTokenSource?.Cancel();
+            _cancelTokenSource?.Dispose();
+            _cancelTokenSource = null;
         }
         
         ~CancelToken()
         {
-            Dispose();
+            Cancel();
         }
     }
 }
